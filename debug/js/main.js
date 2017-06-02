9df579f1137cb0bd0bc7e2e58b16d7a2aeddc47f
fearblackcat <lssbk201@gmail.com>
2017-06-02 14:35:12 +0800

    automatically adjust shapes size in canvas

--- debug/js/main.js
+++ debug/js/main.js
@@ -5,31 +5,18 @@ if (!window.$$) {
 
 $$.Board = class Board {
   constructor(opts) {
-    this._dom = document.createElement("canvas")
-    this._dom.width = opts.w
-    this._dom.height = opts.h
-    document.getElementById("board").appendChild(this._dom)
-    this._ctx = this._dom.getContext("2d")
+    this._dom = document.createElement("canvas");
+    this._dom.width = opts.w;
+    this._dom.height = opts.h;
+    document.getElementById("board").appendChild(this._dom);
+    this._ctx = this._dom.getContext("2d");
   }
 
   paint(elements) {
     elements.map(item => {
-      if(item.type == 'Image' && item.id == 'image1') {
-        this._ctx.beginPath();
-        this._ctx.ellipse((item.x+item.w+this._dom.width-800)/2, (item.y+item.h+this._dom.height-200)/2, item.x+item.w-100, item.y+100, 0, 0, 2 * Math.PI);
-        this._ctx.stroke();
-        this._ctx.fill()
-      } else if(item.type == 'Image' && item.id == 'image2') {
-        this._ctx.beginPath()
-        this._ctx.arc((item.x+item.w+this._dom.width-50)/2, (item.y+item.h+this._dom.height-50)/2, item.h/2, 0, Math.PI*2, false)
-        this._ctx.stroke()
-        this._ctx.fill()
-        //this._ctx.fillText(item.id, item.x, item.y)
-      } else if(item.type == 'Text') {
-        this._ctx.fillRect(item.x, item.y, item.w, item.h)
-        this._ctx.fillText(item.id, item.x, item.y)
-      }
-    })
+      this._ctx.fillRect(item.x, item.y, item.w, item.h);
+      this._ctx.fillText(item.id, item.x, item.y);
+    });
   }
 }
 
@@ -42,16 +29,16 @@ $$.Board = class Board {
    * Main
    */
   let drawResult = (canvas, elements) => {
-    let board = new ($$.Board)(canvas)
-    board.paint(elements)
+    let board = new ($$.Board)(canvas);
+    board.paint(elements);
   }
 
-  const base_height = 400
+  const base_height = 200;
   let getCanvas = (normalized_width) => {
     return {
       w: normalized_width * base_height,
-      h: base_height,
-    }
+      h: base_height
+    };
   }
 
   window.onload = () => {
@@ -62,16 +49,19 @@ $$.Board = class Board {
       { id: 'text1' , type: 'Text' , x: 20 , y: 30 , w: 160, h: 200 },
       { id: 'image1', type: 'Image', x: 200, y: 30 , w: 180, h: 100 },
       { id: 'text2' , type: 'Text' , x: 20 , y: 260, w: 360, h: 40  },
-    ]
+    ];
 
-    let INPUT_W = 4.7
-    canvas = getCanvas(INPUT_W)
+    let INPUT_W = 1.75;
+    let canvas = getCanvas(INPUT_W);
+    drawResult(canvas, elements);
 
-    let DynamicCreative = require('dynamic-creative').DynamicCreative
-    let dc = new DynamicCreative(canvas)
-    dc.add(elements)
-
-    drawResult(canvas, dc.toParams(canvas))
+    let DynamicCreative = require('dynamic-creative').DynamicCreative;
+    let dc = new DynamicCreative(canvas);
+    dc.add(elements);
+    for (let w =  0.5; w < 5; w += 0.3) {
+      canvas = getCanvas(w);
+      drawResult(canvas, dc.toParams(canvas));
+    }
 
   } // end window.onload
 
